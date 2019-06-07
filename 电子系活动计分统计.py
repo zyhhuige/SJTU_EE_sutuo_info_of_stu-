55a5239ff8b124c32c121056bfa8534831776966
yuhui Zeng <47857274+zyhhuige@users.noreply.github.com>
2019-06-07 16:33:45 +0800

    可以拉取交大电子系的素拓统计，及得分情况

---
+++ 电子系活动计分统计.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun  4 13:43:22 2019
+
+@author: Administrator
+"""
+import requests
+import json
+url='https://ssc.sjtu.edu.cn/api/chart/408e24cc/apply/filter'
+#headers={'User-Agent':'Mozilla/5.0'}
+begin_num=117034910001
+jieguo=[]
+for i in range(268):
+    info=[]
+    point=0
+    number=str(begin_num+i)
+    payload={"filter":{"pageSize":50,"pageNum":1,"type":13,"sorts":[],"queries":[],"queryKey":""},"accurateQuery":[{"queId":10902,"queType":2,"searchKey":number}]}
+    t=requests.post(url,data=json.dumps(payload))
+    res=t.json()
+    Id=number
+  
+    total_results=res.get('resultAmount',0)
+    if total_results >0:
+        info.append(Id)
+        name=res.get('result','')[0].get('answers','')[0].get('values')[0].get('value')
+        info.append(name)
+        
+        for j in range(total_results):
+            point+=float(res.get('result','')[j].get('answers','')[5].get('values')[0].get('value'))
+            
+        info.append(point)
+    else:
+        print('none data',end='\t')
+    if info:
+        jieguo.append(info)
+
